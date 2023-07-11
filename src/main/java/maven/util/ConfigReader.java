28458cfd221c9bdb5441ed50dd132b645479196c
Vikramk780 <55432495+Vikramk780@users.noreply.github.com>
2023-07-11 17:06:24 +0530

    Update ConfigReader.java

--- src/main/java/maven/util/ConfigReader.java
+++ src/main/java/maven/util/ConfigReader.java
@@ -14,7 +14,7 @@ public class ConfigReader {
 		
 		prop = new Properties();
 		try {
-			FileInputStream ip = new FileInputStream("./test/resources/Config/properties/config.properties");
+			FileInputStream ip = new FileInputStream("src/test/resources/Config/properties/config.properties");
 			prop.load(ip);
 
 		} catch (FileNotFoundException e) {
